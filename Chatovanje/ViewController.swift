9d6e7919b2858a3172381cb0d10bb969ab420be1
Predrag Jevtic <predragjevtic@gmail.com>
2018-02-22 14:19:00 +0100

    Initial Commit

---
+++ Chatovanje/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatovanje
+//
+//  Created by Predrag Jevtic on 2/22/18.
+//  Copyright © 2018 Predrag Jevtic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
