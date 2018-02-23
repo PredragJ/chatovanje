bde53c790fd0b90d77ca04b2ba1571ad37e75123
Predrag Jevtic <predragjevtic@gmail.com>
2018-02-23 14:28:53 +0100

    End of 4.

---
+++ Chatovanje/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Chatovanje
+//
+//  Created by Predrag Jevtic on 2/23/18.
+//  Copyright © 2018 Predrag Jevtic. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Chatovanje/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Chatovanje
+//
+//  Created by Predrag Jevtic on 2/23/18.
+//  Copyright © 2018 Predrag Jevtic. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
